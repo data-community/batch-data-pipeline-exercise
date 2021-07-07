5c457879255fe40f58ed9e1d170e758dc42457b9
Yuan Zhang <zhangyuan@users.noreply.github.com>
2021-07-07 19:18:42 +0800

    remove last_event_time

--- examples/process_orders_sqls.py
+++ examples/process_orders_sqls.py
@@ -105,14 +105,12 @@ AND '{{ ts }}' >= dim_orders.start_time AND '{{ ts }}' < dim_orders.end_time
 AND (earliest_orders.status <> dim_orders.status);
 
 WITH ordered_stg_orders as (
-    SELECT *, ROW_NUMBER() OVER(PARTITION BY id,status ORDER BY event_time) rn,
-    LAST_VALUE(event_time) OVER(PARTITION BY id,status ORDER BY event_time) last_event_time
+    SELECT *, ROW_NUMBER() OVER(PARTITION BY id,status ORDER BY event_time) rn
     FROM stg_orders
     order by id, event_time
-), distinct_stg_orders as (
+), deduplicate_stg_orders as (
     select id, status, event_time, 
     event_time as start_time,
-    last_event_time as end_time,
     ROW_NUMBER() OVER(PARTITION BY id ORDER BY event_time) rn
     from ordered_stg_orders where ordered_stg_orders.rn = 1
 ) ,new_records as (select 
@@ -121,7 +119,7 @@ WITH ordered_stg_orders as (
     current_orders.event_time,
     current_orders.event_time as start_time,
     coalesce(next_orders.event_time, '%s') as end_time
-from distinct_stg_orders current_orders left join distinct_stg_orders next_orders
+from deduplicate_stg_orders current_orders left join deduplicate_stg_orders next_orders
 on current_orders.id = next_orders.id and current_orders.rn = next_orders.rn -  1
 ) 
 INSERT INTO dim_orders(order_id, status, event_time, processed_time, start_time, end_time)
